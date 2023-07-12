52fc61fe99af346cdca2a304fbd30d51b13e1ea4
jrarkaan <rakajanitraa@hotmail.com>
2023-07-12 10:48:14 +0700

    base: add logging

--- rabbitmq/v1.5/rabbitmq.go
+++ rabbitmq/v1.5/rabbitmq.go
@@ -1,8 +1,12 @@
 package v1_5
 
-import "github.com/streadway/amqp"
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+)
 
 func InitRabbitMQConnection(url string) (error, *amqp.Connection) {
+	fmt.Println("Connection RabbitMQ")
 	connection, errorConnection := amqp.Dial(url)
 	if errorConnection != nil {
 		return errorConnection, nil
